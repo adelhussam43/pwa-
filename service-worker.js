82598072ef13b368bb1e1ef41c19fb35e9229a7e
adelhussam43 <124381175+adelhussam43@users.noreply.github.com>
2024-12-17 18:07:02 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
     '/index.html',
     '/style.css',
     '/app.js',
-    '/images/Philadelphia-University-Jordan-logo.jpg'
+    '/Philadelphia-University-Jordan-logo.jpg'
 ];
 
 // Install event
